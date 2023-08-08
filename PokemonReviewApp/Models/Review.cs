623193224f52a9641abcfe77d2d712789ae2f204
Bishop <christianiradukunda6@gmail.com>
2023-08-08 05:14:19 +0200

    added the one to many relationship

--- PokemonReviewApp/Models/Review.cs
+++ PokemonReviewApp/Models/Review.cs
@@ -4,7 +4,9 @@
     {
         public int Id { get; set; } 
         public string Title { get; set; }
-        public string Text { get; set; } 
+        public string Text { get; set; }
+        public Reviewer Reviewer { get; set; }
+        public Pokemon Pokemon { get; set; }
     }
 
 }
--- PokemonReviewApp/Models/Reviewer.cs
+++ PokemonReviewApp/Models/Reviewer.cs
@@ -4,7 +4,8 @@
     {
         public int Id { get; set; } 
         public string FirstName { get; set; }
-        public string LastName { get; set; } 
-            
+        public string LastName { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+
     }
 }
