989368cf533bce5d48ba0c5a077c6149da430733
Bishop <christianiradukunda6@gmail.com>
2023-08-09 23:06:59 +0200

    created the database context

--- PokemonReviewApp/Models/PokemonCategory.cs
+++ PokemonReviewApp/Models/PokemonCategory.cs
@@ -7,5 +7,6 @@ namespace PokemonReviewApp.Models
         public int PokemonId { get; set; }
         public int CategoryId { get; set; }
         public Pokemon Pokemon { get; set; }
+        public Category Category { get; set; }
     }
 }
