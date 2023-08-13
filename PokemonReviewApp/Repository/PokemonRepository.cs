25082b011478d55dd0cb52d71cd9f1f4f007e43c
Bishop <christianiradukunda6@gmail.com>
2023-08-13 06:49:10 +0200

    implemented the new changes from the interface

--- PokemonReviewApp/Repository/PokemonRepository.cs
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -12,9 +12,30 @@ namespace PokemonReviewApp.Repository
             _context = context;
                 
         }
+
+        public Pokemon GetPokemon(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetPokemonRating(int pokeid)
+        {
+            throw new NotImplementedException();
+        }
+
         public ICollection<Pokemon> GetPokemons()
         {
             return _context.Pokemon.OrderBy(p => p.Id).ToList(); 
+        }
+
+        public bool PokemonExists(int pokeid)
+        {
+            throw new NotImplementedException();
         }
     }
 }
