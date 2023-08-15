bedffd7499392722ebd2c5505f0ed8b86980d874
Bishop <christianiradukunda6@gmail.com>
2023-08-15 09:05:44 +0200

    added the icategoryrepository interface

---
+++ PokemonReviewApp/Interfaces/IcategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using PokemonReviewApp.Models;
+using System.ComponentModel;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface IcategoryRepository
+    {
+        ICollection<Category>GetCategories();
+        Category GetCategory(int id);  
+        ICollection<Pokemon> GetPokemonByCategory(int categoryId);   
+        bool CategoriesExists(int id);  
+    }
+}
---
+++ PokemonReviewApp/Repository/CategoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class CategoryRepository : IcategoryRepository
+    {
+        public bool CategoriesExists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Category> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Pokemon> GetPokemonByCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
