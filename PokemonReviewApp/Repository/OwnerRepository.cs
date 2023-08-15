f30ba79b7b5a4bc01b8ada7000c9c6da6e6a14b8
Bishop <christianiradukunda6@gmail.com>
2023-08-15 17:36:45 +0200

    added the OwnerRepository

---
+++ PokemonReviewApp/Repository/OwnerRepository.cs
@@ -0,0 +1,33 @@
+﻿using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        public Owner GetOwner(int ownerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Owner> GetOwnerOfAPokemon(int pokeid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Owner> GetOwners()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Pokemon> GetPokemonByOwner(int ownerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OwnerExists(int ownerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
