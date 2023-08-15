fe7240ee63957919c20967b56c7fc30ef175e030
Bishop <christianiradukunda6@gmail.com>
2023-08-15 23:16:36 +0200

    added the ReviewRepositry

---
+++ PokemonReviewApp/Repository/ReviewRepository.cs
@@ -0,0 +1,37 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReviewRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            
+            
+        }
+        public ICollection<Review> GetReviews()
+        {
+            return _context.Reviews.ToList();
+        };
+
+        public Review GetReview(int reviewId)
+        {
+           return  _context.Reviews.Where(r => r.Id == reviewId).FirstOrDefault();
+        }
+
+        public ICollection<Review> GetReviewOfAPokemon(int pokeId)
+        {
+            return _context.Reviews.Where(r => r.Pokemon.Id == pokeId).ToList();    
+        }
+
+        public bool ReviewExists(int reviewId)
+        {
+            return _context.Reviews.Any(p => p.Id == reviewId);
+        }
+    }
+}
