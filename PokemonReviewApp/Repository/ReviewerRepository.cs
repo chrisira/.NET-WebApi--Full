c7a314d1cc95fcc2f4f73b72349874123aca23ea
Bishop <christianiradukunda6@gmail.com>
2023-08-16 07:56:35 +0200

    added the Reviewer repository

---
+++ PokemonReviewApp/Repository/ReviewerRepository.cs
@@ -0,0 +1,28 @@
+﻿using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class ReviewerRepository : IreviewerRepository
+    {
+        public Reviewer GetReviewer(int reviewerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Reviewer> GetReviewers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Reviewer> GetReviewsByReviewer(int reviewerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ReviewExists(int reviewerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
