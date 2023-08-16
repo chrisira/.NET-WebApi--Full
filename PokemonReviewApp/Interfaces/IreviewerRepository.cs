772ddfa30c16978299665d64f939dfd6f4774f03
Bishop <christianiradukunda6@gmail.com>
2023-08-16 07:55:26 +0200

    done implementing the IReviewerRepository interface

---
+++ PokemonReviewApp/Interfaces/IreviewerRepository.cs
@@ -0,0 +1,14 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface IreviewerRepository
+    {
+        ICollection<Reviewer> GetReviewers();
+
+        Reviewer GetReviewer(int reviewerId);   
+        ICollection<Reviewer> GetReviewsByReviewer(int reviewerId);
+        bool ReviewExists(int reviewerId);  
+
+    }
+}
