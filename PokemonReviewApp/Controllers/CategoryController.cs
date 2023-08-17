9e37cf82088f3ad79d6ba3f3c861e46d9590a86c
Bishop <christianiradukunda6@gmail.com>
2023-08-17 15:11:16 +0200

    Added the create category endpoint in category controler

--- PokemonReviewApp/Controllers/CategoryController.cs
+++ PokemonReviewApp/Controllers/CategoryController.cs
@@ -72,5 +72,45 @@ namespace PokemonReviewApp.Controllers
             return Ok(pokemons);
         }
 
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public IActionResult CreateCategory([FromBody] CategoryDto categoryCreate)
+        {
+            //checking if the categoryCreate is null
+           if(categoryCreate == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // cheking if the category name exists
+            var category = _categoryRepository.GetCategories()
+                .Where(c => c.Name.Trim().ToUpper() == categoryCreate.Name.TrimEnd().ToUpper()).FirstOrDefault();   
+
+            if (category == null)
+            {
+                ModelState.AddModelError("", "category already exists");
+                return StatusCode(422, ModelState);
+            }
+
+            // checking if data is valid
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);  
+            }
+            var categoryMap = _mapper.Map<Category>(categoryCreate);
+
+            if (!_categoryRepository.CreateCategory(categoryMap))
+            {
+                ModelState.AddModelError("", "Something went wrong while trying to save category");
+                return StatusCode(500, ModelState);
+            }
+
+            return Ok("category save successfull");
+
+        }
+
+
     }
 }
